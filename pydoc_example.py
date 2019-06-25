e36d9ffdb4927dd5ecb54440c81d1869d8ec4961
jRobinson33 <eldringethmoore@yahoo.com>
2019-06-25 09:48:00 -0700

    comments showing commands on how to use pydoc

---
+++ pydoc_example.py
@@ -0,0 +1,18 @@
+# pydoc examples
+#6/25/2019
+
+#  C:\>python -m pydoc       this looks at the documentation of pydoc
+#  C:\>python -m pydoc math  this looks at the documentation of the math module
+
+#  C:\>python -m pydoc -k ftp  does a keyword search for ftp
+
+"""
+C:\>mkdir pydoc_demo
+
+C:\>cd pydoc_demo
+
+C:\pydoc_demo>python -m pydoc -w json
+wrote json.html
+
+C:\pydoc_demo>start json.html
+"""
